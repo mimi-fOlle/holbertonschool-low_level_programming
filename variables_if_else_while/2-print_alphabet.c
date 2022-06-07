9603137150c4e1085a8bd524b7b26d35aa9f897b
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-07 18:35:56 +0200

    putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* putchar - Write a character of unsigned char type, to stdout
+* @a: Alphabet starts
+* @z: Alphabet ends
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar (ch, '\n');
+	}
+	return (0);
+}
