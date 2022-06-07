78d69b3346500885a85908fb41d332a010b9effc
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-07 10:43:52 +0200

    hello printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,12 +2,13 @@
 #include <string.h>
 
 /**
-* main - An int is a keyword that references an integer data type
-*  
-* Return: Standard output
-**/
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-	char str[] = "with proper grammar, but the outcome is a piece of art,";
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
