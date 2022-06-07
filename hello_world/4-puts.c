b71f8f0053161de16fff02d11494d1b03f822e2a
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-07 09:41:10 +0200

    hello puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* puts - writes the given string to the standard output stream stdout
+*
+* Return: 
+*/
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
