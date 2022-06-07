059ed7077e07406826c01630e012efd9cc57a4eb
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-07 21:09:30 +0200

    alphabets

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -2,9 +2,24 @@
 
 /**
 * main - Entry point
+* putchar - Write a character of unsigned char type, to stdout
 *
 * Return: Always 0 (Success)
 */
 
 int main(void)
+{
+        char ch1;
+	char ch2;
 
+        for (ch1 = 'a'; ch1 <= 'z'; ch1++)
+        {
+                putchar (ch1);
+        }
+	for (ch2 = 'A'; ch2 <= 'Z'; ch2++)
+	{
+		putchar (ch2);
+	}
+        putchar (10);
+        return (0);
+}	
