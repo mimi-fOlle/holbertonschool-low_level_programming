7a0199bd41bd47d128ecc131a4a459e462cb1817
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-08 10:31:26 +0200

    hex

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar (n);
+	}
+	for (n = 97; n <= 102; n++)
+	{
+		putchar (n);
+	}
+	putchar (10);
+	return (0);
+}
