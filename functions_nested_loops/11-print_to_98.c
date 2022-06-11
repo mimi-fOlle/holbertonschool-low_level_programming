4a2fb3474562cc4cf7ba5ba425bdd95943adb534
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-11 16:24:46 +0200

    98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,12 +2,28 @@
 #include "main.h"
 
 /**
-*
-*
-*
+* print_to_98 - print number till 98
+* @n: number
+* @i: number
 */
 
 void print_to_98(int n)
 {
+	int i;
 
+	if (n < 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+		printf("%i, ",i);
+		}
+	}
+	else if (n > 98)
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%i, ",i);
+		}
+	}
+	printf("%i\n",i);
 }
