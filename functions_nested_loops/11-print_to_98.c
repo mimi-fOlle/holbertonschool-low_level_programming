ddfbe7a9ab19d0570f4118859a5caca0506381c9
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-11 16:35:43 +0200

    98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i < 98; i++)
 		{
-		printf("%i, ",i);
+			printf("%i, ",i);
 		}
 	}
 	else if (n > 98)
@@ -25,5 +25,5 @@ void print_to_98(int n)
 			printf("%i, ",i);
 		}
 	}
-	printf("%i\n",i);
+	printf("%i\n",n);
 }
