dc95f765a1b58be380feb0fec383551353c5dcbb
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-13 21:33:48 +0200

    square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,12 +2,30 @@
 #include "main.h"
 
 /**
-*
-*
+* print_square - print a square
+* @size: the size of the square
+* x - loop
+* y - loop
 *
 */
 
 void print_square(int size)
 {
+	int x;
+	int y;
+
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size; y++)
+		{
+			_putchar('#');
+		}
+		_putchar(10);
+
+	}
+	if (size <= 0)
+	{
+		_putchar(10);
+	}
 
 }
