9cf5cc28eba4fec58d1226da810c8b90bfa8203c
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-13 17:45:10 +0200

    diagonals

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*
+*
+*
+*/
+
+void print_triangle(int size)
--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -2,12 +2,25 @@
 #include "main.h"
 
 /**
-*
-*
+* print_diagonal - draw a diagonal line
+* @n: number
 *
 */
 
 void print_diagonal(int n)
 {
 
+	if (n <= 0)
+	{
+		_putchar(10);
+	}
+	else if (n > 0)
+	{
+		for (n = 1; n < 100; n++)
+		{
+		_putchar(32);
+		}
+	}
+	_putchar(92);
+	_putchar(10);
 }
