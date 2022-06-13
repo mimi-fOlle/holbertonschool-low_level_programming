e54ce8d06928e1554aaf4e2bc352a03bc99114cd
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-13 14:10:48 +0200

    line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -3,7 +3,7 @@
 
 /**
 * print_line - draw straight line
-* n - number
+* @n: number
 *
 */
 
@@ -12,12 +12,11 @@ void print_line(int n)
 	if (n <= 0)
 	{
 		_putchar(10);
-	}
 
 	for (n = 1; n < 100; n++)
 	{
 		_putchar('_');
-
+	}
 	}
 	_putchar(10);
 }
