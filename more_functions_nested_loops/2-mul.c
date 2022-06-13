7134ff20c6fa558f464593701bf0adc0414be625
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-13 10:01:03 +0200

    mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* mul - multiply integer numbers
+* @a: number
+* @b: number
+* result - ouput result
+*
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
