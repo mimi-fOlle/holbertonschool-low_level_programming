5e79e6f86253de8791111518b286ea32007eb705
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-14 07:33:39 +0200

    triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -2,9 +2,33 @@
 #include "main.h"
 
 /**
-*
-*
+* print_triangle - print a triangle
+* @size: size of the triangle
+* x - loop
+* y - loop
 *
 */
 
 void print_triangle(int size)
+{
+	int x;
+	int y;
+
+	for (x = 1; x <= size; x++)
+	{
+		for (y = size; y >= 1; y--)
+		{
+			if ((size - x) <= (size - y))
+			{
+				_putchar('#');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+			_putchar(10);
+	}
+	if (size <= 0)
+		_putchar(10);
+}
