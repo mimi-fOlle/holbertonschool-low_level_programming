2aac777a57789675f793b94ca0b18c727b7c35e0
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-14 21:19:04 +0200

    puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,13 +2,19 @@
 #include "main.h"
 
 /**
-*
-*
-*
+* _puts - print a string, followed by a new line, to stdout
+* @str: character string to be written
 *
 */
 
 void _puts(char *str)
 {
+	int t;
 
+	for (t = 0; str[t] != '\0'; t++)
+	{
+		_putchar(str[t]);
+	}
+
+	_putchar(10);
 }
