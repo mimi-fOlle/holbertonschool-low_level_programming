de39590b870331c16d2c145efbec79cfef870b5e
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-14 09:28:01 +0200

    temp swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+* swap_int - swaps the values of two integers
+* @a: value
+* @b: value
+* temp - value temp
+*
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
