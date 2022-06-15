4eebb93c8e94dc802c024d6d4c5cbad149996b80
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-15 09:28:47 +0200

    print array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -3,17 +3,21 @@
 
 /**
 * print_array - print n elements of an array of integers
-*
+* @a: value
+* @n: value
 *
 */
 
 void print_array(int *a, int n)
 {
-	for (n = 0; n < 5; n++)
+
+	int z = 300;
+
+	for (n = 0; n < z; n++)
 	{
 		printf("%i", a[n]);
 
-		if (n < 4)
+		if (n < (z - 1))
 			printf(", ");
 	}
 	printf("\n");
