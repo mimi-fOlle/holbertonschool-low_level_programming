23eae5456fa4d1f3469a5da6592e62e6aff857a5
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-17 18:28:58 +0200

    strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strncpy - copy a string
+* @dest: pointer to the destination array where the contetn is to be copied
+* @src: the string to be copied
+* @n : number of input
+*
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[j] = src[j];
+	}
+
+	for (; j < n; j++)
+	{
+		dest[j] = '\0';
+	}
+
+	return (dest);
+}
