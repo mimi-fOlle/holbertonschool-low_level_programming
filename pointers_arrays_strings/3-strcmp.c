9a5b4f4ec51dcfee71351abed2df19e2a0b14d75
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-18 10:08:19 +0200

    strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strcmp - compares two strings (s1, S2)
+* @s1: string 1
+* @s2: string 2
+*
+* Return: Always 0
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int j;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	if (i > j)
+		return (15);
+	else if (i < j)
+		return (-15);
+	return (0);
+}
