d765271371d3aa18f22a5bedfa189695d7064c60
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-18 10:12:24 +0200

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -19,8 +19,8 @@ int _strcmp(char *s1, char *s2)
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 	if (i > j)
-		return (15);
+		return (13);
 	else if (i < j)
-		return (-15);
+		return (-13);
 	return (0);
 }
