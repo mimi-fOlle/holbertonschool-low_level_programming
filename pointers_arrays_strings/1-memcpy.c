40bff0e9dbb6222c67990584d94a6ce267fb8514
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-19 23:11:04 +0200

    memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -14,9 +14,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i == n; i++)
+	for (i = 0; i < n; i++)
 	{
-		src[i] = dest[i];
+		dest[i] = src[i];
 	}
 	return (dest);
 }
