e2b01016fe0c416e56fd6601243d1a13c3786e0c
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-22 09:17:10 +0200

    mynewnameis

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
