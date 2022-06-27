a06768554285911dd74b8ce7ed7ced1e5a41894d
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-27 18:55:45 +0200

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* struct dog - new definition
+* @name: name
+* @age: age
+* @owner: owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
