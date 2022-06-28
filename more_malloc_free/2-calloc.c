648dc268a3acc14b17e990fd363e57976d6a4974
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-28 14:39:14 +0200

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,27 +3,27 @@
 #include "main.h"
 
 /**
-* _calloc - allocate memory for an array
-* @nmemb: element
-* @size: size
+* _calloc - allocate memory for an array with malloc
+* @nmemb: number of elements to be allocted
+* @size: size of elements
 *
-* Return: NULL if fails
+* Return: return a pointer to the allocated memory, or NULL if fails
 */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *ptr;
+	unsigned int i;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
-	{
 		return (NULL);
-	}
-	ptr = malloc(size);
+
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
-	for (nmemb = 0; ptr[nmemb]; nmemb++)
-		;
+
+	for (i = 0; i < nmemb * size; i++)
+		ptr[i] = 0;
+
 	return (ptr);
 }
