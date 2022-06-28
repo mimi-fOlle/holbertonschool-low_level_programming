6e72909ba82ae043c548c6d3adb9a86fca8dd5ad
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-28 09:36:39 +0200

    new_dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,10 +16,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
+	{
+		free(new_dog);
 		return (NULL);
+	}
 	new_dog->name = name;
 	new_dog->age = age;
 	new_dog->owner = owner;
-	free(new_dog);
 	return (new_dog);
 }
