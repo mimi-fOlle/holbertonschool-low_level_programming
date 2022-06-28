58d45d24e9945b52806c751561579059031c0db7
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-28 07:52:29 +0200

    print_dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,11 +15,11 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
-/**		if (d->age == 0)
-*			printf("Age: (nil)\n");
+		if (d->age == 0 || d->age != 0)
+/**			printf("Age: \n");
 *		else
-*			printf("Age: %f\n", d->age);
-*/		if (d->owner == NULL)
+*/			printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", d->owner);
