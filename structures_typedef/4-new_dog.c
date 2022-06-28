8b9abf744d4614c087acfe4ab99fa96f1711b49e
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-28 09:32:05 +0200

    new_dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* new_dog - create a new_dog
+* @name: member
+* @age: member
+* @owner: member
+*
+* Return: return to pointer
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return (new_dog);
+}
