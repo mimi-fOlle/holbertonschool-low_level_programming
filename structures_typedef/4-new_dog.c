66438fb982030a2f339954187706de123b7cf35f
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-06-28 10:15:08 +0200

    new_dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,35 +15,37 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, na, ow;
 
-	dog_t *new_dog = malloc(sizeof(new_dog));
+	dog_t *hot_dog;
 
-	if (new_dog == NULL || !(name) || !(owner))
+	hot_dog = malloc(sizeof(*hot_dog));
+
+	if (hot_dog == NULL || !(name) || !(owner))
 	{
-		free(new_dog);
+		free(hot_dog);
 		return (NULL);
 	}
 	for (na = 0; name[na]; na++)
 		;
 	for (ow = 0; owner[ow]; ow++)
 		;
-	new_dog->name = malloc(na + 1);
-	new_dog->owner = malloc(ow + 1);
-	if (!(new_dog->name) || !(new_dog->owner))
+	hot_dog->name = malloc(na + 1);
+	hot_dog->owner = malloc(ow + 1);
+	if (!(hot_dog->name) || !(hot_dog->owner))
 	{
-		free(new_dog->name);
-		free(new_dog->owner);
-		free(new_dog);
+		free(hot_dog->name);
+		free(hot_dog->owner);
+		free(hot_dog);
 		return (NULL);
 	}
 	for (i = 0; i < na; i++)
-		new_dog->name[i] = name[i];
-	new_dog->name[i] = '\0';
+		hot_dog->name[i] = name[i];
+	hot_dog->name[i] = '\0';
 
-	new_dog->age = age;
+	hot_dog->age = age;
 
 	for (i = 0; i < ow; i++)
-		new_dog->owner[i] = owner[i];
-	new_dog->owner[i] = '\0';
+		hot_dog->owner[i] = owner[i];
+	hot_dog->owner[i] = '\0';
 
-	return (new_dog);
+	return (hot_dog);
 }
