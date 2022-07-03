7e3d1f78fc38e1ffe9bb5dc1e2654927b9579b9b
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-03 15:11:41 +0200

    int_index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,5 +13,17 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	(*cmp)(array[size]);
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]) == 1)
+			return (i);
+	}
+	return (-1);
 }
