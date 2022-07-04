684a2b8ab382dc7b6e5dc0838bda5eebb8d83c5d
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-04 20:44:51 +0200

    print string

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -27,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s%s", va_arg(ap, char*), separator);
 		if (i == n - 1)
 			printf("%s", va_arg(ap, char*));
-		if (i < n && va_arg(apcpy, char*) == NULL)
+		if (i < n -1 && va_arg(apcpy, char*) == NULL)
 			printf("(nil)");
 
 	}
