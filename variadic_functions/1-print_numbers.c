248c49874d12330a851512c3ffaba996fb1c0709
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-04 13:19:55 +0200

    print numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,14 +16,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(ap, n);
 
-	if (separator == NULL)
-		printf("%i", va_arg(ap, int));
-
 	for (i = 0; i < n; i++)
 	{
 		if (i < n - 1)
 			printf("%i%s", va_arg(ap, int), separator);
 		else
+			printf("%i", va_arg(ap, int));
+		if (separator == NULL)
 			printf("%i", va_arg(ap, int));
 	}
 	va_end(ap);
