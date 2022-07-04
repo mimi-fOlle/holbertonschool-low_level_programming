854ae1cf554a714b84fd85a4dad80355a8716a9a
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-04 12:20:56 +0200

    calc

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "3-calc.h"
 
@@ -19,17 +20,20 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
+	
 	if (*argv[2] != 43 && *argv[2] != 45 && *argv[2] != 42
 && *argv[2] != 47 && *argv[2] != 37)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+	
 	if ((*argv[2] == 47 || *argv[2] == 37) && atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
+	
 	result = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
 	printf("%i\n", result);
 	return (0);
