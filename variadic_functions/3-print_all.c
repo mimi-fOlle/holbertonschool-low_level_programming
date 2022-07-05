d079458b797c1173593a888d2c28dbfb2edc6c17
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-05 20:24:44 +0200

    print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 #include "variadic_functions.h"
 
 /**
@@ -8,7 +9,60 @@
 * @format: a list of types of arguments passed to the function
 */
 
+void charprint(va_list va)
+{
+	printf("%c", va_arg(va, int));
+}
+
+void intprint(va_list va)
+{
+	printf("%d", va_arg(va, int));
+}
+
+void floatprint(va_list va)
+{
+	printf("%f", va_arg(va, double));
+}
+
+void stringprint(va_list va)
+{
+	printf("%s", va_arg(va, char *));
+}
+
 void print_all(const char * const format, ...)
 {
+	va_list ap, apcpy;
+	int runargs, runFormat;
 
+	struct type_t fm[] = {
+		{'c', charprint},
+		{'i', intprint},
+		{'f', floatprint},
+		{'s', stringprint},
+	};
+
+	va_start(ap, format);
+
+	runFormat = 0;
+	runargs = 0;
+
+	while (fm[runargs].t_short)
+	{
+		while (fm[runFormat].t_short)
+		{
+			if (format[runFormat] == fm[runargs].t_short)
+			{
+				fm[runargs].typeprint(ap);
+
+			if (fm[runargs + 1].t_short)
+				printf(", ");
+			}
+			runFormat++;
+		}
+		runargs++;
+		runFormat = 0;
+	}
+	va_end(apcpy);
+	va_end(ap);
+	printf("\n");
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,23 @@
 #ifndef MAIN_H_
 #define MAIN_H_
-int _putchar(char c);
+#include <stdarg.h>
+
+/**
+* struct type - definition
+* @c: char
+* @i: integer
+* @f: float
+* @s: char *
+*/
+struct type_t
+{
+	char t_short;
+	void (*typeprint)(va_list);
+};
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 #endif
