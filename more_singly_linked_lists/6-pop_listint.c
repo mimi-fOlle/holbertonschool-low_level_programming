08721279cf347f8ebbcec95c2b268246b82ab771
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-13 13:46:14 +0200

    print listint

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+* pop_listint - deletes the head node of a listint_t linked list
+* @head: node
+*
+* Return: the head node's date (n), or 0 if the linked list is empty
+*/
+
+int pop_listint(listint_t **head)
+{
+
+}
