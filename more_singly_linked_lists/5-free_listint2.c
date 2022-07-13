cbcea97bc1f6968c16c93e62550b830b66b27677
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-13 13:27:32 +0200

    free head = null

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* free_listint2 - free a listint_t list
+* @head: node to be freed
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *clear;
+
+	if (head == NULL || *head == NULL)
+		exit(0);
+	while (*head != NULL)
+	{
+		clear = *head;
+		*head = (*(head))->next;
+		free(clear);
+	}
+	*head = NULL;
+}
