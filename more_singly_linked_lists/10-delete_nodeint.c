18ac40851690fbdca1c2daaa386867769c718d21
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-14 15:46:09 +0200

    delete at index

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -9,3 +9,30 @@
 */
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *tmp;
+	listint_t *current = *head;
+
+	if (!head || !*head)
+		return (-1);
+
+	else if (index == 0)
+	{
+		*head = (*head)->next;
+		free(current);
+		return (1);
+	}
+
+	for (i = 0; i < index - 1; i++)
+	{
+		current = current->next;
+		if (current == NULL)
+			return (-1);
+	}
+	tmp = current->next;
+	current->next = tmp->next;
+	free(tmp);
+
+	return (1);
+}
