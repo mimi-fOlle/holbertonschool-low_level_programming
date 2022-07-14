a49669dd41fce0a5d0ea1c3d56add8203996cf78
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-14 15:00:15 +0200

    insert nodeint at index

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+* delete_nodeint_at_index - deletes the node at index index of listint_t list
+* @head: node
+* @index: the index of the node that should be deleted. Starts at 0
+*
+* Return: 1 if it succeeded. -1 if it failed
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -11,19 +11,30 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *tmp;
+	listint_t *new_node, *current = *head;
 
-	if (idx == '\0')
-		return(NULL);
-
-	tmp = (listint_t *)malloc(sizeof(listint_t));
-	if (tmp == NULL)
-	{
+	new_node = (listint_t *)malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
+
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = current;
+		*head = new_node;
+		return (*head);
 	}
-	
-	tmp->n = tmp;
-	tmp->next = (*head)->next;
-	(*head)->next = tmp;
-	return (idx);
+	while (idx > 1)
+	{
+		current = current->next;
+		idx--;
+		if (!current)
+		{
+			free(new_node);
+			return (NULL);
+		}
+	}
+	new_node->next = current->next;
+	current->next = new_node;
+	return (new_node);
 }
