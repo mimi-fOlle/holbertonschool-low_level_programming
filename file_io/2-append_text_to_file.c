350ad8e6ba82c1c7f3abdc8e099b5d38f89a04cb
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-19 20:20:10 +0200

    append text to file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -11,3 +11,28 @@
 */
 
 int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+
+		w = write(fd, text_content, len);
+		if (w == -1)
+			return (-1);
+	}
+	else
+		fd = open(filename, O_CREAT);
+
+	close(fd);
+	return (1);
+}
