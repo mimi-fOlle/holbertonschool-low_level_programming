3bf6d5002835df205ee3d172f29101f8ae62306e
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-19 18:12:28 +0200

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,20 +19,19 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 6000);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[len])
+	if (text_content != NULL)
 	{
-		len++;
-		if (text_content == NULL)
-		{
-			fd = open(filename, O_CREAT, 6000);
-		}
-		else
-			write(fd, text_content, len);
+
+		while (text_content[len])
+			len++;
+		write(fd, text_content, len);
 	}
 	close(fd);
+	if ((close(fd)) == -1)
+		return (-1);
 	return (1);
 }
