a6ca44065b60122dfa3b2a48a307d32204a45b9d
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-19 14:21:55 +0200

    create a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+* create_file - create a file
+* @filename: name of the file to create
+* @text_content: is a NULL terminated string to write to the file
+*
+* Return: 1 if success; -1 on failure
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, 6000);
+	if (fd == -1)
+		return (-1);
+
+	while (text_content[len])
+		len++;
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (-1);
+	}
+	else
+		write(fd, text_content, len);
+
+	close(fd);
+	return (1);
+}
