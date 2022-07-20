e705631279c91e9bd537266a208ded4f6152e8bc
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-20 09:36:16 +0200

    print doubly linked list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* print_distint - prints all the elements of a distint_t list
+* @h: head node
+*
+* Return: the number of nodes
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *tmp = h;
+	unsigned int i = 0;
+
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
+}
