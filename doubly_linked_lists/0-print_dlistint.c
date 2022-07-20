761fc4cd63694d183d3129873db5b6e793f7b03f
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-20 09:37:20 +0200

    print doubly linked list

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-* print_distint - prints all the elements of a distint_t list
+* print_dlistint - prints all the elements of a dlistint_t list
 * @h: head node
 *
 * Return: the number of nodes
