c9680a1d16b35c56a41f74d2c11ab20890d36085
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-21 15:12:38 +0200

    cp file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,38 +1,74 @@
 #include "main.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
-*
-*
-*
+* file_error - while file is error
+* @file_from: file from
+* @file_to: file to
+* @argv: argument vector
 */
 
-int main()
+void file_error(int file_from, int file_to, char *argv[])
 {
-	FILE *file_from, *file_to;
-	char NAME_OF_THE_FILE[1000];
-
-	file_to = open(NAME_OF_THE_FILE, O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	if (file_to == NULL)
+	if (file_from == -1)
 	{
-		dprintf(NAME_OF_THE_FILE[0], "Error: Can't write to 
-NAME_OF_THE_FILE\n");
-		exit (99);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+* main - Entry point
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: 0 if success
+*/
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, error_file;
+	int count, nwr;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
 	}
 
-	file_from = open(NAME_OF_THE_FILE, O_RDONLY);
-	if (file_from == NULL)
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	file_error(file_from, file_to, argv);
+
+	count = 1024;
+
+	while (count == 1024)
 	{
-		dprintf(NAME_OF_THE_FILE[0], "Error: Can't read from file 
-NAME_OF_THE_FILE\n");
-		exit (98);
+		count = read(file_from, buf, 1024);
+		if (count == -1)
+			file_error(-1, 0, argv);
+		nwr = write(file_to, buf, count);
+		if (nwr == -1)
+			file_error(0, -1, argv);
 	}
 
-	close(file_to);
-	close(file_from);
-	if (file_to != NULL || file_from != NULL)
+	error_file = close(file_from);
+	error_file = close(file_to);
+	if (error_file == -1)
 	{
-		dprintf(NAME_OF_THE_FILE[0], "Error: Can't close fd
-FD_VALUE\n");
-		exit (100);
-		return (0);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
 }
