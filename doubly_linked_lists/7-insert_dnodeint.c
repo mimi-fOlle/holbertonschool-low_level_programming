127188d961b013cef33ef5230d3caf8aff1f51c5
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-22 11:59:13 +0200

    delete nodes

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -48,41 +48,38 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			tmp = tmp->next;
 			count++;
 		}
-
 	}
 	return (new_node);
 }
-
-/**
-	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	
-	if (idx == 0)
-	{
-		new_node->next = tmp;
-		*h= new_node;
-		return (*h);
-	}
-	while (idx > 1)
-	{
-		tmp = tmp->next;
-		idx--;
-		if (!tmp)
-		{
-			free(new_node);
-			return (NULL);
-		}
-	}
-	new_node->next = tmp->next;
-	new_node->prev = tmp;
-	if (tmp->next != NULL)
-	{
-		new_node->next->prev = new_node;
-	}
-	tmp->next = new_node;
-	return (*h);
-}
+/*
+*	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+*	if (new_node == NULL)
+*		return (NULL);
+*
+*	new_node->n = n;
+*	if (idx == 0)
+*	{
+*		new_node->next = tmp;
+*		*h= new_node;
+*		return (*h);
+*	}
+*	while (idx > 1)
+*	{
+*		tmp = tmp->next;
+*		idx--;
+*		if (!tmp)
+*		{
+*			free(new_node);
+*			return (NULL);
+*		}
+*	}
+*	new_node->next = tmp->next;
+*	new_node->prev = tmp;
+*	if (tmp->next != NULL)
+*	{
+*		new_node->next->prev = new_node;
+*	}
+*	tmp->next = new_node;
+*	return (*h);
+*
 */
--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -44,31 +44,31 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	return (-1);
 }
-/**
-
-	if (*head == NULL || head == NULL)
-		return (-1);
-
-	for (i = 1; i < index && tmp != NULL; i++)
-	{
-		tmp = tmp->next;
-		if (tmp == NULL)
-			return (-1);
-	}
-	if (index == 0)
-	{
-		*head = (*head)->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-		free(tmp);
-		return (1);
-	}
-	else if (tmp != NULL)
-	{
-		tmp->prev->next = tmp->next;
-		tmp->next->prev = tmp->prev;
-		free(tmp);
-	}
-	return (1);
-}
+/*
+*
+*	if (*head == NULL || head == NULL)
+*		return (-1);
+*
+*	for (i = 1; i < index && tmp != NULL; i++)
+*	{
+*		tmp = tmp->next;
+*		if (tmp == NULL)
+*			return (-1);
+*	}
+*	if (index == 0)
+*	{
+*		*head = (*head)->next;
+*		if (*head != NULL)
+*			(*head)->prev = NULL;
+*		free(tmp);
+*		return (1);
+*	}
+*	else if (tmp != NULL)
+*	{
+*		tmp->prev->next = tmp->next;
+*		tmp->next->prev = tmp->prev;
+*		free(tmp);
+*	}
+*	return (1);
+*
 */
