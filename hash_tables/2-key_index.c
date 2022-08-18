a4eeb772d823b797089fe7d68955e42c77b5db5e
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-08-18 21:23:04 +0200

    redo task 2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -13,11 +13,8 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash = 0;
-	unsigned long int i;
-
-	for (i = 0; i < size; i++)
-		hash = (hash + key[i]) % size;
-
-	return (hash);
+	if (!key || size <= 0)
+		return (0);
+	
+	return (hash_djb2(key) % size);
 }
