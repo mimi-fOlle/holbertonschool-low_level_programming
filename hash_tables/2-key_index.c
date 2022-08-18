1aee70c80e4db853a6fc8b2d1e0a8e84b1cbb833
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-08-18 14:29:33 +0200

    add task 2

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * key_index - Gives the index of a key
+  * @key: The key
+  * @size: The size of the array of the hash table
+  *
+  * Return: The index at which the key/value pair should stored 
+  * in the array of the hash table
+  */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+	unsigned int index;
+
+	hash = 0;
+	index = 0;
+
+	while (key && key[index])
+	{
+		hash = (hash + key[index] % size);
+		index++;
+	}
+	return (hash);
+}
