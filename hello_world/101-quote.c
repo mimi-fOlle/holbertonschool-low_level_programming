bf08d0beec25c82010f08d924da86e8fa7eae689
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-02 10:52:32 +0200

    add 101-quote.c

---
+++ hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+  * main - Prints the phrase with standard error
+  * Return: 1 if succuss
+  */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);	
+}
