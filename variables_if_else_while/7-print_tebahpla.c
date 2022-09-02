a8da14e2e8d6ff812e765c0a871641a772332b53
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-02 11:20:54 +0200

    modified

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -11,9 +11,8 @@ int main(void)
 	char ch;
 
 	for (ch = 'z'; ch >= 'a'; ch--)
-	{
 		putchar (ch);
-	}
+	
 	putchar (10);
 	return (0);
 }
--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -13,10 +13,11 @@ int main(void)
 	for (n = 48; n <= 57; n++)
 	{
 		putchar (n);
+		
 		if (n != 57)
 		{
-		putchar (44);
-		putchar (32);
+			putchar (44);
+			putchar (32);
 		}
 	}
 	putchar (10);
